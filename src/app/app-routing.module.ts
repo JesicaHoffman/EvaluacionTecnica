f8343f5abfa31e9a5b07748a04c28a71fc1c3bd1
Jesica Hoffman <jhoffman@mobydigital.com>
2021-09-02 15:57:35 -0300

    Tabla estatica y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostlistComponent } from './postlist/postlist.component';
+import { PostdetailComponent } from './details/postdetail/postdetail.component';
+import { Error404Component } from './error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "posts", component: PostlistComponent },
+  { path: "post:id", component: PostdetailComponent },
+  { path: "404", component: Error404Component }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

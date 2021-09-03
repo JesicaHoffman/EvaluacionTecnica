4b447f822782b47989c1aec78d7df06f52ffb83b
Jesica Hoffman <jhoffman@mobydigital.com>
2021-09-03 15:07:02 -0300

    Finalizado

--- src/app/details/comments/comments.component.ts
+++ src/app/details/comments/comments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { PostService } from 'src/app/post.service'; 
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
@@ -9,33 +10,44 @@ import { ActivatedRoute } from '@angular/router';
   providers: [PostService]
 })
 export class CommentsComponent implements OnInit {
-   comments:any=[]
-  @Input() fecha1:any
+   comments:any;
+   commentsFilter:any
+   filteredComments:any
+
+   
+  @Input() fecha1:any;
+  @Input() id:any;
   @Output() fechaActual:EventEmitter<any>;
   
+  getDate(){
+    this.fechaActual.emit(this.fecha1);
+}
   
-   
-  constructor(
+   constructor(
     public postservice:PostService,
     private activatedRoute: ActivatedRoute
-  ) { 
+              ) { 
      this.fechaActual= new EventEmitter();
-  }
+      }
 
-  ngOnInit(): void {
+  ngOnInit() {
     
-    const id= this.activatedRoute.snapshot.paramMap.get("id")
-    
-
-    this.postservice.getCommentsById(id)
+     this.postservice.getComments()
     .subscribe(data => {
-      console.log(data)
-      this.comments=data
-    })
-  }
+      this.comments=data;
+      console.log(this.comments)
+      
+      this.commentsFilter= this.comments.filter((res:any) => {
+        if(res.postId == this.id){
+        return  res
+        }
+  
+      });
 
-  getDate(){
-      this.fechaActual.emit(this.fecha1)
-  }
+   });
+    
+   }
+
+ 
 
 }
